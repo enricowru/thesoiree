ca4cce1e190b6544194fee7cf58e9bb5b230042a
enricowru <enricosuarez777@gmail.com>
2025-06-04 23:39:48 +0800

    Update MainStack.js

--- navigation/MainStack.js
+++ navigation/MainStack.js
@@ -1,17 +1,19 @@
-  import React from "react"; 
-  import { createNativeStackNavigator } from "@react-navigation/native-stack"; //This navigator allows for transitioning between screens where each new screen is placed on top of a stack ur welcome
-  import Login from "../screens/Login";
-  import QRScanner from "../screens/QRScanner";
-  import BottomTabNavigator from "./BottomTabNavigator";
+import React from "react"; 
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/Login";
+import QRScanner from "../screens/QRScanner";
+import BottomTabNavigator from "./BottomTabNavigator";
+import ReviewsScreen from "../screens/ReviewsScreen"; // ✅ Added import
 
-  const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
-  export default function MainStack() {
-    return (
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="MainTabs" component={BottomTabNavigator} />
-        <Stack.Screen name="QRScanner" component={QRScanner} />
-      </Stack.Navigator>
-    );
-  }
+export default function MainStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="MainTabs" component={BottomTabNavigator} />
+      <Stack.Screen name="QRScanner" component={QRScanner} />
+      <Stack.Screen name="Reviews" component={ReviewsScreen} /> {/* ✅ Added screen */}
+    </Stack.Navigator>
+  );
+}
